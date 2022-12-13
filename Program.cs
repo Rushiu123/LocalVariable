33446fefff3092416feb1e3e69641936ac20a643
Rushiu123 <rushikesh.ubalkar@gmail.com>
2022-12-13 16:37:06 +0530

    variable

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace LocalVariables236;
+{
+    class Program
+    {
+       Static void Main(string[] args)
+       {
+          Console.WriteLine("HelloWorld");
+         //classname variable=new classname()//
+         //caculator calculator =new calcultor();
+         ////"20"  -> 20
+         /////calculatr.additiontwoNumbers();
+          TypesOfVariables.StudentAge();
+          Console.ReadLine();
+
+
+       }
+
+
+    }
+}
+
+ 
+
+   
+
+
+
+
---
+++ TypesOfVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.studentAge;
+
+namespace LocalVariables236
+{
+    class TypesOfVariables
+    {
+        public void StudentAge()
+        { 
+            int age = 0;
+            age=  age + 10;
+            Console.WriteLine("Student age is ;" + age);
+            Console.ReadLine();
+
+        }
+    }
+}
